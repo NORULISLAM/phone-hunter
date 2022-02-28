55929074428ace72b912f1a37617ffbdd45c577e
user <juyelr16@gmail.com>
2022-03-01 08:32:03 +0900

    second commit

--- js/index.js
+++ js/index.js
@@ -8,5 +8,29 @@ const searchPhoon = () => {
     // console.log(url);
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data));
+        .then(data => displaySearchResult(data.data));
+}
+
+const displaySearchResult = phones => {
+    const searchResult = document.getElementById('search-result');
+    phones.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+            <img src="${phone.image}" class="card-img-top w-50" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.brand}</p>
+            </div>
+            
+            <div class="d-grid justify-content-end">
+
+            <button class="btn btn-dark text-white rounded-pill me-md-2" type="button">Details</button>
+             </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
