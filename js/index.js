d3fa3c199e51353f08f8b2dca7a138da7cddbe3b
user <juyelr16@gmail.com>
2022-03-01 19:02:14 +0900

    third commit

--- js/index.js
+++ js/index.js
@@ -14,12 +14,12 @@ const searchPhoon = () => {
 const displaySearchResult = phones => {
     const searchResult = document.getElementById('search-result');
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card">
-            <img src="${phone.image}" class="card-img-top w-50" alt="...">
+            <img src="${phone.image}" class="card-img-top w-30" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">${phone.brand}</p>
@@ -27,10 +27,36 @@ const displaySearchResult = phones => {
             
             <div class="d-grid justify-content-end">
 
-            <button class="btn btn-dark text-white rounded-pill me-md-2" type="button">Details</button>
-             </div>
+              <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-dark text-white rounded-pill me-md-2" type="button">Details</button>
+            </div>
         </div>
         `;
         searchResult.appendChild(div);
     })
+}
+const loadPhoneDetails = phoneId => {
+    console.log(phoneId);
+    const url = ` https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(phoneData => displayPhoneDetails(phoneData.data));
+}
+const displayPhoneDetails = phoneDtails => {
+    console.log(phoneDtails);
+    const phoneDetails = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src='${phoneDtails.image}' class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${phoneDtails.name}</h5>
+        <p class="card-text">${phoneDtails.releaseDate}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+    </ul>
+    `;
+    phoneDetails.appendChild(div);
 }
